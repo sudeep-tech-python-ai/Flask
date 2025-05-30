2ec561e2bdef354810d0858403bdefea80ed4c88
shindesudeep <206251900+sudeep-tech-python-ai@users.noreply.github.com>
2025-05-30 12:56:10 +0530

    flask2

--- flask/jinja.py
+++ flask/jinja.py
@@ -2,7 +2,13 @@
 ## Variable rule
 ### Jinja 2 Template Engine
 
-from flask import Flask ,render_template,request
+### Jinja 2 Template Engine
+'''
+{{ }} expressions to print output in html
+{%...%} conditions, for loops
+{#...#} this is for comments'''
+
+from flask import Flask ,render_template,request,redirect,url_for
 '''
 It creates an instance of the Flask class,
 which will be your WSGI(Web Server GateWay Interface) application.
@@ -24,18 +30,48 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/submit',methods=['GET','POST'])
-def submit():
-    if request.method=='POST':
-        name=request.form['name']
-        return f'Hello {name}!'
-    return render_template('form.html')
 
 ## variable rule
-@app.route('/success/<score>')
+@app.route('/success/<int:score>')
 def success(score):
-    return "The marks you got is "+score
+    res=""
+    if score>=50:
+        res="PASSED"
+    else:
+        res="FAILED"
+    return render_template('result.html',results=res)
 
+@app.route('/successres/<int:score>')
+def successres(score):
+    res=""
+    if score>=50:
+        res="PASSED"
+    else:
+        res="FAILED"
+    exp={'score':score,'res':res}
+    return render_template('result1.html',results=exp)
+
+@app.route('/successif/<int:score>')
+def successif(score):
+    return render_template('result.html',results=score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return render_template('result.html',results=score)
+
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data_science=float(request.form['datascience'])
+
+        total_score=(science+maths+c+data_science)/4
+    else:
+        return render_template('getresult.html')
+    return redirect(url_for('successres',score=total_score))
 
 
 if __name__=="__main__":
