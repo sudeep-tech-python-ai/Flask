23535de856486cb325bac65dfbc7eb6a8c1d377a
shindesudeep <206251900+sudeep-tech-python-ai@users.noreply.github.com>
2025-05-30 12:04:27 +0530

    flask1

---
+++ flask/jinja.py
@@ -0,0 +1,42 @@
+### Building Url Dynamically 
+## Variable rule
+### Jinja 2 Template Engine
+
+from flask import Flask ,render_template,request
+'''
+It creates an instance of the Flask class,
+which will be your WSGI(Web Server GateWay Interface) application.
+'''
+
+### WSGI Application
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<html><h1>Welcome to the flask course.</h1></html>"
+
+@app.route("/index",methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route('/submit',methods=['GET','POST'])
+def submit():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
+
+## variable rule
+@app.route('/success/<score>')
+def success(score):
+    return "The marks you got is "+score
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
